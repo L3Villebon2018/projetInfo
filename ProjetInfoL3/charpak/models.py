e72a911654f556da95c5db5582101a7770576621
paris-ci <jovart.arthur@gmail.com>
2018-10-11 10:55:45 +0200

    Mise en place d'un modele de base pour Etudiant, Formation et Ecole.

--- ProjetInfoL3/charpak/models.py
+++ ProjetInfoL3/charpak/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-#TODO: Create your models here.
+
+class Ecole(models.Model):
+    nom = models.CharField(max_length=150)
+
+
+class Formation(models.Model):
+    nom = models.CharField(max_length=150)
+    domaine = models.CharField(max_length=150)
+    type = models.CharField(max_length=150)
+
+    ecole = models.ForeignKey(Ecole, on_delete=models.CASCADE, default=None, blank=True, null=True)
+
+
+class Etudiant(models.Model):
+    nom = models.CharField(max_length=150)
+    prenom = models.CharField(max_length=150)
+    photo = models.ImageField()
+
+    promo = models.PositiveSmallIntegerField()
+    parain = models.ForeignKey('Etudiant', on_delete=models.CASCADE, default=None, blank=True, null=True)
+
+    profil_bac = models.CharField(max_length=50)
+
+    formation = models.ForeignKey(Formation, on_delete=models.CASCADE, default=None, blank=True, null=True)
+    statut = models.CharField(max_length=50)
