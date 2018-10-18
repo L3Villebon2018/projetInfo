e719ce0422b7400afe23fa0a5879671611b47c53
Arthur <jovart.arthur@gmail.com>
2018-10-18 20:28:47 +0200

    Correction de tous les liens de matthéo

--- ProjetInfoL3/charpak/urls.py
+++ ProjetInfoL3/charpak/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('profil', views.index_profil),
     path('FAQ', views.index_FAQ),
     path('fil_actu', views.index_fil_actu),
-    path('fil_actu/nouveau_post', views.nouveau_post_fil_actu)
+    path('fil_actu/nouveau_post', views.nouveau_post_fil_actu, name="fil-actu-nouveau-post")
 
 ]
