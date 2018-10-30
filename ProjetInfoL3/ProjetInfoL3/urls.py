640703ce85ee15f9deff37fe931a59946375a6ac
Arthur <jovart.arthur@gmail.com>
2018-10-30 16:41:16 +0100

    Mise à jour des répertoires d'envoi des images

--- ProjetInfoL3/ProjetInfoL3/urls.py
+++ ProjetInfoL3/ProjetInfoL3/urls.py
@@ -13,14 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import (path,
                          include)
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('charpak.urls')),
     url(r'^hijack/', include('hijack.urls', namespace='hijack')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
