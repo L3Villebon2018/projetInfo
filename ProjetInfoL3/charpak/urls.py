971f6cc110ddfb252761f6954cdaa97ad551d93b
Marine <marine.kazemi@gmail.com>
2018-11-01 19:45:12 +0100

    2éme page de la FAQ finis (pour la V0)

--- ProjetInfoL3/charpak/urls.py
+++ ProjetInfoL3/charpak/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('arborescence', views.index_arborescence, name="arborescence-index"),
     path('profil/<int:etudiant_id>', views.index_profil, name="profil-etudiant"),
     path('FAQ', views.index_FAQ, name="FAQ-index"),
+    path('FAQ/astuces', views.astuces_FAQ, name="FAQ-astuces"),
     path('fil_actu', views.index_fil_actu, name="fil-actu-index"),
     path('fil_actu/nouveau_post', views.nouveau_post_fil_actu, name="fil-actu-nouveau-post"),
     #path('fil_actu/<int:post_id>', views.nouveau_commentaire, name="fil-actu-details-post"),
