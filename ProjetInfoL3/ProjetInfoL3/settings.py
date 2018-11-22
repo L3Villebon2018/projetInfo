a633849cd1c05b796d149b00fc39eacfe6433205
Arthur <jovart.arthur@gmail.com>
2018-11-22 13:08:49 +0100

    Mise à jour de la BDD

--- ProjetInfoL3/ProjetInfoL3/settings.py
+++ ProjetInfoL3/ProjetInfoL3/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'ProjetInfoL3.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db_tests.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
